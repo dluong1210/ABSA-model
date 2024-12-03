97e243b24aa59f49032b6e08e44fd3f287a0fc7e
Duc Hoang Pham <hoangggp@gmail.com>
2024-12-03 20:33:43 +0700

    add code for write log in training process

--- src/train.py
+++ src/train.py
@@ -3,13 +3,12 @@ from sklearn.metrics import accuracy_score, f1_score
 from tqdm import tqdm
 import pickle
 
-
-def train_model(model, criterion, optimizer, data_loader_train, 
-                data_loader_valid, epochs, device, task='detection'):
+def train_model(model, criterion, optimizer, data_loader_train,
+                data_loader_valid, epochs, device, task='detection', path_log='test.txt'):
     model.to(device)  # Đưa mô hình vào GPU/CPU
 
     f1_best = 0
-    
+
     for epoch in range(epochs):
         model.train()
         total_loss = 0
@@ -28,7 +27,7 @@ def train_model(model, criterion, optimizer, data_loader_train,
             else:
                 loss_dict = model(input_ids, attention_mask, targets, test=False)
                 loss = loss_dict['BCE_loss'] + loss_dict['contrastive_loss']
-            
+
             # Backward pass
             loss.backward()
             optimizer.step()
@@ -41,14 +40,18 @@ def train_model(model, criterion, optimizer, data_loader_train,
                                      "%s: %.5f" % (l, loss_dict[l].detach()) for l in loss_dict
                                  ]))
 
-        print(f"Epoch {epoch + 1}/{epochs}, Training Loss: {total_loss / len(data_loader_train)}")
+        epoch_traning_loss = f"Epoch {epoch + 1}/{epochs}, Training Loss: {total_loss / len(data_loader_train)}"
+        print(epoch_traning_loss)
+        # save log 
+        log = open(path_log, 'a')
+        log.write(epoch_traning_loss + "\n")
 
         # Evaluate on validation set
         model.eval()
         valid_loss = 0
         all_preds = []
         all_targets = []
-        
+
         with torch.no_grad():
             pbar = tqdm(data_loader_valid, total=len(data_loader_valid))
             for batch in pbar:
@@ -61,7 +64,7 @@ def train_model(model, criterion, optimizer, data_loader_train,
                     probs = model(input_ids, attention_mask, targets, test=True)
                 else:
                     probs = model(input_ids, attention_mask, targets, test=True)
-                    
+
                 loss = criterion(probs, targets)
                 valid_loss += loss.item()
 
@@ -80,20 +83,42 @@ def train_model(model, criterion, optimizer, data_loader_train,
 
         if isinstance(criterion, torch.nn.CrossEntropyLoss):
             accuracy = accuracy_score(all_targets, all_preds)
-            print(f"Validation Accuracy: {accuracy:.4f}")
+            accuracy_log = f"Validation Accuracy: {accuracy:.4f}"
+            print(accuracy_log)
+            log.write(accuracy_log + "\n")
         else:
-            f1 = f1_score(all_targets, all_preds, average='micro')
-            print(f"Validation F1 Score: {f1:.4f}")
+            f1 = f1_score(all_targets, all_preds, average='macro')
+            f1_log = f"Validation F1 Score: {f1:.4f}"
+            print(f1_log)
+            log.write(f1_log + "\n")
+        
+        validation_loss = f"Validation Loss: {valid_loss / len(data_loader_valid)}"
+        
+        print(validation_loss)
+        log.write(validation_loss + "\n")
 
-        print(f"Validation Loss: {valid_loss / len(data_loader_valid)}")
-
-        # get result 
-        f1 = f1_score(all_targets, all_preds, average='micro')
-        if f1 > f1_best: 
+        # compare to save best result 
+        f1 = f1_score(all_targets, all_preds, average='macro')
+        precision = precision_score(all_targets, all_preds, average='macro')
+        recall = recall_score(all_targets, all_preds, average='macro')
+        if f1 > f1_best:
             f1_best = f1
-            print(f'best in epoch: {epoch+1}/{epochs} - best f1 score: {f1_best}')
+            best_log = f'best in epoch: {epoch+1}/{epochs} - best f1 score: {f1_best}'
+            print(best_log)
+            log.write(best_log + "\n")
+
+            # write precision, recall, f1 for best epoch 
+            log.write(f'f1: {f1} - recall: {recall} - precision: {precision}' + "\n")
+
+            log.write('save best model .... \n')
             # save best model
-            name_model_pkl = 'model_aspect.pkl' if isinstance(criterion, nn.BCEWithLogitsLoss) else 'model_sentiment.pkl'
-            with open(name_model_pkl, 'wb') as f:
-                pickle.dump(model, f)
+            with open('model_aspect.pkl', 'wb') as f:
+                pickle.dump(model_aspect_detection, f)
+
+        # evaluate on test set
+        if task == 'detection':
+            evaluate_model(model, aspect_test_loader, device, short_format=True, log_path=path_log)
+        if task == 'classification':
+            evaluate_model(model, sentiment_test_loader, device, short_format=True, log_path=path_log)
+        log.close()
 
