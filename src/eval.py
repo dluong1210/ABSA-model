a33e76918810a1bc5e2a19f6023a73405b3fda92
Duc Hoang Pham <hoangggp@gmail.com>
2024-12-03 20:35:10 +0700

    add code for write log in evaluate process

--- src/eval.py
+++ src/eval.py
@@ -2,11 +2,14 @@ import torch
 from tqdm import tqdm
 from sklearn.metrics import f1_score, precision_score, recall_score, classification_report
 
-def evaluate_model(model, data_loader, device, short_format=False):
+def evaluate_model(model, data_loader, device, short_format=False, log_path='test.txt'):
     model.eval()
     all_preds = []
     all_labels = []
+
+    log = open(log_path, 'a') 
     
+
     with torch.no_grad():
         pbar = tqdm(data_loader, total=len(data_loader))
         for batch in pbar:
@@ -17,11 +20,11 @@ def evaluate_model(model, data_loader, device, short_format=False):
             if isinstance(model, AspectDetection):
                 probs = model(input_ids, attention_mask, labels, test=True)
                 preds = (torch.sigmoid(probs) >= 0.5).int()  # Multi-label
-                
+
             else:
                 probs = model(input_ids, attention_mask, labels, test=True)
                 preds = torch.argmax(probs, dim=1)  # Multi-class
-                
+
             all_preds.append(preds.cpu())
             all_labels.append(labels.cpu())
 
@@ -38,8 +41,14 @@ def evaluate_model(model, data_loader, device, short_format=False):
     recall_micro = recall_score(all_labels, all_preds, average='micro')
 
     if short_format:
-        print(f"Macro F1 Score: {f1:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}")
-        print(f"Micro F1 Score: {f1_micro:.4f}, Precision: {precision_micro:.4f}, Recall: {recall_micro:.4f}")
+        print(f"Macro score - F1 : {f1:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}")
+        print(f"Micro score - F1: {f1_micro:.4f}, Precision: {precision_micro:.4f}, Recall: {recall_micro:.4f}")
+        macro_score = f"Macro Score - F1: {f1:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}"
+        micro_score = f"Micro score - F1: {f1_micro:.4f}, Precision: {precision_micro:.4f}, Recall: {recall_micro:.4f}"
+
+        log.write(macro_score + "\n")
+        log.write(micro_score + "\n")
+    log.close()
 
     if not short_format:
         print(classification_report(all_labels, all_preds))
